cd95a9eeeeb0972ce2a57a3e5e3bbcf3692e1fe0
Chaplygin Vladislav <metro_flava@mail.ru>
2021-05-24 21:38:26 +0300

    add model Task, register in admin, create sample ListView

--- to_do/base/views.py
+++ to_do/base/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import ListView
+from .models import Task
 
 
-def task_list(request):
-    return HttpResponse('To Do')
+class TaskList(ListView):
+    model = Task
