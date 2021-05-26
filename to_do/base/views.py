b9488eb855898142be0ba24e471a67652dd8c356
Chaplygin-vlad <metro_flava@mail.ru>
2021-05-26 17:32:17 +0300

    add update and delete form

--- to_do/base/views.py
+++ to_do/base/views.py
@@ -1,6 +1,12 @@
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.contrib.auth.views import LoginView
 from django.urls import reverse_lazy
 from .models import Task
+
+
+class Login(LoginView):
+    template_name = 'base/'
+
 
 
 class TaskList(ListView):
@@ -17,4 +23,16 @@ class TaskDetail(DetailView):
 class TaskCreate(CreateView):
     model = Task
     fields = '__all__'
+    success_url = reverse_lazy('tasks')
+
+
+class TaskUpdate(UpdateView):
+    model = Task
+    fields = '__all__'
+    success_url = reverse_lazy('tasks')
+
+
+class TaskDelete(DeleteView):
+    model = Task
+    context_object_name = 'task'
     success_url = reverse_lazy('tasks')
